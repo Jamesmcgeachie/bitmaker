8992c3a97de1df842c26694271104f6bfa23ecb1
James McGeachie <jamesmcgeachie10@gmail.com>
2015-08-05 23:55:31 -0400

    Updated exercise 4 to add BitMaker line

--- exercise4.rb
+++ exercise4.rb
@@ -5,10 +5,12 @@
 x = 0
 while x < 100
     x += 1
-        if x % 3 == 0 
-        puts "Bit"
+        if (x % 3 == 0) && (x % 5 == 0)
+        puts "BitMaker"
         elsif x % 5 == 0
         puts "Maker"
+        elsif (x % 3 == 0)
+        puts "Bit"
         else
         puts x
     end
