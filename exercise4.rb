3c73ca71628d819870679cac61d1f7eccd914243
James McGeachie <jamesmcgeachie10@gmail.com>
2015-08-05 23:11:12 -0400

    add exercise4.rb

---
+++ exercise4.rb
@@ -0,0 +1,19 @@
+# Used while control structure for the count to 100.
+# Used if/elsif statement and modulus to find if divisible by 3 and 5
+# It worked!
+
+x = 0
+while x < 100
+    x += 1
+        if x % 3 == 0 
+        puts "Bit"
+        elsif x % 5 == 0
+        puts "Maker"
+        else
+        puts x
+    end
+end
+
+
+
+
